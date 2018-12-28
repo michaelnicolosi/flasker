f0f1381997b53180f41eb984278a5f542ecf0d79
michaelnicolosi <mike@mikenicolosi.com>
2018-12-28 13:18:26 -0500

    Add files via upload

---
+++ my_flask_app/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'This is an article with some real text in it for surely sure.',
+            'author': 'Michael Nicolosi',
+            'create_date': '12-28-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'This is an article with some real text in it for surely sure.',
+            'author': 'Michael Nicolosi',
+            'create_date': '12-29-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'This is an article with some real text in it for surely sure.',
+            'author': 'Michael Nicolosi',
+            'create_date': '12-30-2018'
+        }
+    ]
+    return articles
